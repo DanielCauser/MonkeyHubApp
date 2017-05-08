39e9812c5490ed46c7e042771c9d6c6d1458b984
DanielCauser <danielcauser@gmail.com>
2017-05-08 09:50:24 -0300

    Monkey hub Binding CLASS

---
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace MonkeyHubApp.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+
+        //private string _descricao;
+
+        //public string Descricao
+        //{
+        //    get { return _descricao; }
+        //    set { SetProperty(ref _descricao, value); }
+        //}
+        //public MainViewModel()
+        //{
+        //    Descricao = "Olá mundo! Eu estou aqui!";
+
+        //    Task.Delay(3000).ContinueWith(async t =>
+        //    {
+        //        Descricao = "Meu texto mudou";
+        //        for (int i = 1; i <= 10; i++)
+        //        {
+        //            Descricao = $"Meu texto mudou {i}";
+        //            await Task.Delay(1000);
+        //        }
+        //    });
+        //}
+    }
+}
